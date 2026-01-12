72096e64d98f36ed0954176c67dd906d5f4c5241
snehapahuja <snehapahuja604@gmail.com>
2026-01-12 12:25:46 +0530

    Day 5 committed

---
+++ Day 5/GreetingApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using GreetingLibrary;
+
+namespace GreetingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your name: ");
+            string name1= Console.ReadLine();
+            string greet = GreetingHelper.GetGreeting(name1);
+            Console.WriteLine(greet);
+        }
+    }
+}
---
+++ Day 5/GreetingLibrary/GreetingHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace GreetingLibrary
+{
+    public class GreetingHelper
+    {
+        public static string GetGreeting(string name) 
+        {
+            if (name == "")
+            {
+                return "Hello, Guest!";
+            }
+            return $"Hello, {name}";
+        }
+    }
+}
