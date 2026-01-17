bec8d794ae012755842be01d816438255f846218
snehapahuja <snehapahuja604@gmail.com>
2026-01-17 12:13:50 +0530

    Assessment commit

---
+++ Assessments/Week2Assessment/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace Week2Assessment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] policyHolderNames = new string[5];
+            decimal[] annualPremiums = new decimal[5];
+
+
+
+            //Input
+            for (int i = 0; i < policyHolderNames.Length; i++)
+            {
+                Console.Write($"Enter the name of policy holder {i + 1}: ");
+                policyHolderNames[i] = Console.ReadLine();
+                Console.Write($"Enter the annual premium for {policyHolderNames[i]}: ");
+                annualPremiums[i] = Convert.ToDecimal(Console.ReadLine());
+
+                if (policyHolderNames[i] == "")
+                {
+                    Console.WriteLine("Policy holder name cannot be empty. Try Again.");
+                    i--;
+                }
+                if (annualPremiums[i] < 0)
+                {
+                    Console.WriteLine("Annual premium cannot be negative. Try Again.");
+                    i--;
+                }
+            }
+
+            //2.Processing
+            decimal totalPremium = 0;
+            decimal minPremium = annualPremiums[0];
+            decimal maxPremium = annualPremiums[0];
+
+            for (int i = 0; i < 5; i++)
+            {
+                totalPremium += annualPremiums[i];
+
+                if (annualPremiums[i] < minPremium)
+                    minPremium = annualPremiums[i];
+
+                if (annualPremiums[i] > maxPremium)
+                    maxPremium = annualPremiums[i];
+            }
+
+            decimal averagePremium = totalPremium / 5;
+
+
+            string category = "";
+
+            // Output Report
+            Console.WriteLine("\nInsurance Premium Summary:");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("\n Name \t Premium \t Category");
+            for (int i = 0; i < policyHolderNames.Length; i++)
+            {
+                if (annualPremiums[i] < 10000)
+                {
+                    category = "Low";
+                }
+                else if (annualPremiums[i] >= 10000 && annualPremiums[i] < 25000)
+                {
+                    category = "Medium";
+                }
+                else
+                {
+                    category = "Premium";
+                }
+                Console.WriteLine($"{policyHolderNames[i].ToUpper()} \t {annualPremiums[i]:F2} \t {category}");
+            }
+            Console.WriteLine($"\nTotal Premium: {totalPremium:F2}");
+            Console.WriteLine($"Average Premium: {averagePremium:F2}");
+            Console.WriteLine($"Highest Premium: {maxPremium:F2}");
+            Console.WriteLine($"Lowest Premium: {minPremium:F2}");
+        }
+    }
+}
--- Day 9/SaleAnalyzer/Program.cs
+++ Day 9/SaleAnalyzer/Program.cs
@@ -4,75 +4,74 @@
     {
         static void Main(string[] args)
         {
-            
 
-                decimal[] weekSale = new decimal[7];
-                for (int i = 0; i < weekSale.Length; i++)
+
+            decimal[] weekSale = new decimal[7];
+            for (int i = 0; i < weekSale.Length; i++)
+            {
+                Console.Write($"Enter Sales Day {i + 1}: ");
+                weekSale[i] = Convert.ToDecimal(Console.ReadLine());
+
+                if (weekSale[i] < 0)
                 {
-                    Console.Write($"Enter Sales Day {i + 1}: ");
-                    weekSale[i] = Convert.ToDecimal(Console.ReadLine());
-                  
-                    if (weekSale[i] < 0)
-                    {
-                        Console.WriteLine("Sales can't be negative. Try Again.");
-                        i--; 
-                    }
-
-                }
-                decimal totalSales = 0;
-                for (int i = 0; i < weekSale.Length; i++)
-                {
-                    totalSales += weekSale[i];
-                }
-                decimal averageSales = totalSales / weekSale.Length;
-
-                decimal lowestsale = weekSale.Min();
-                int positionLowest = Array.IndexOf(weekSale, lowestsale) + 1;
-                decimal highestsale = weekSale.Max();
-                int positionHighest = Array.IndexOf(weekSale, highestsale) + 1;
-
-                string[] category = new string[7];
-                for (int i = 0; i < weekSale.Length; i++)
-                {
-                    if (weekSale[i] < 5000)
-                    {
-                        category[i] = "Low";
-                    }
-                    else if (weekSale[i] >= 5000 && weekSale[i] < 15000)
-                    {
-                        category[i] = "Medium";
-                    }
-                    else
-                    {
-                        category[i] = "High";
-                    }
-                }
-
-
-                Console.WriteLine("\nWeekly Sales Report:");
-                Console.WriteLine($"\nTotal Sales for the week: {totalSales:C}");
-                Console.WriteLine($"Average Daily Sale:{averageSales:C} ");
-                Console.WriteLine($"\nLowest Sale: {lowestsale:C} on Day {positionLowest}");
-                Console.WriteLine($"Highest Sale: {highestsale:C} on Day {positionHighest}");
-
-                int count = 0;
-                for (int i = 0; i < weekSale.Length; i++)
-                {
-                    if (weekSale[i] > averageSales)
-                    {
-                        count += 1;
-                    }
-                }
-                Console.WriteLine($"Days with sales greater than weekly average:{count}");
-
-
-                Console.WriteLine("\nDay-wise Sales Category:");
-                for (int i = 0; i < category.Length; i++)
-                {
-                    Console.WriteLine($"Day {i + 1}: {category[i]}");
+                    Console.WriteLine("Sales can't be negative. Try Again.");
+                    i--;
                 }
 
             }
+            decimal totalSales = 0;
+            for (int i = 0; i < weekSale.Length; i++)
+            {
+                totalSales += weekSale[i];
+            }
+            decimal averageSales = totalSales / weekSale.Length;
+
+            decimal lowestsale = weekSale.Min();
+            int positionLowest = Array.IndexOf(weekSale, lowestsale) + 1;
+            decimal highestsale = weekSale.Max();
+            int positionHighest = Array.IndexOf(weekSale, highestsale) + 1;
+
+            string[] category = new string[7];
+            for (int i = 0; i < weekSale.Length; i++)
+            {
+                if (weekSale[i] < 5000)
+                {
+                    category[i] = "Low";
+                }
+                else if (weekSale[i] >= 5000 && weekSale[i] < 15000)
+                {
+                    category[i] = "Medium";
+                }
+                else
+                {
+                    category[i] = "High";
+                }
+            }
+
+
+            Console.WriteLine("\nWeekly Sales Report:");
+            Console.WriteLine($"\nTotal Sales for the week: {totalSales}");
+            Console.WriteLine($"Average Daily Sale:{averageSales} ");
+            Console.WriteLine($"\nLowest Sale: {lowestsale} on Day {positionLowest}");
+            Console.WriteLine($"Highest Sale: {highestsale} on Day {positionHighest}");
+
+            int count = 0;
+            for (int i = 0; i < weekSale.Length; i++)
+            {
+                if (weekSale[i] > averageSales)
+                {
+                    count += 1;
+                }
+            }
+            Console.WriteLine($"Days with sales greater than weekly average:{count}");
+
+
+            Console.WriteLine("\nDay-wise Sales Category:");
+            for (int i = 0; i < category.Length; i++)
+            {
+                Console.WriteLine($"Day {i + 1}: {category[i]}");
+            }
+
         }
     }
-
+}
