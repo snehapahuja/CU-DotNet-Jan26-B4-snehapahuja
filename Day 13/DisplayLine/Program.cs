4c56ad5e1bd101edc7963e1b3a402c07e987f524
snehapahuja <snehapahuja604@gmail.com>
2026-01-21 16:16:19 +0530

    Day 13 commit

---
+++ Day 13/DisplayLine/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DisplayLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DisLine();
+            DisLine('$');
+            DisLine(ch: '+', length: 60);
+        }
+        static void DisLine(char ch = '-', int length = 40)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(ch);
+            }
+            Console.WriteLine();
+        }
+    }
+}
