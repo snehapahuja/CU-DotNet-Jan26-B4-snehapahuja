6a67058de935d74955f3a67e2b7a7ca1c7cc20f6
snehapahuja <snehapahuja604@gmail.com>
2026-01-23 12:31:31 +0530

    Day 15 commit

---
+++ Day 15/01Exercise/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _01Exercise
+{
+    class Height
+    {
+        public int Feet { get; set; }
+        public double Inches { get; set; }
+
+        public Height()
+        {
+            Feet = 0;
+            Inches = 0;
+        }
+
+        public Height(int Feet, double Inches)
+        {
+            this.Feet = Feet;
+            this.Inches = Inches;
+        }
+
+        public Height AddHeights(Height h1)
+        {
+            int f = this.Feet + h1.Feet;
+            double inch = this.Inches + h1.Inches;
+
+            f += (int)inch / 12;
+            inch = inch % 12;
+
+            Height h3 = new Height(f, inch);
+
+            return h3;
+        }
+
+        public override string ToString()
+        {
+            return $"Height = {Feet} feet {Inches:F1} inches";
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Height person1 = new Height()
+            {
+                Feet = 5,
+                Inches = 6.5,
+            };
+
+            Height person2 = new Height()
+            {
+                Feet = 7,
+                Inches = 9,
+            };
+
+            Height person3 = new Height();
+            person3 = person2.AddHeights(person1);
+
+            Console.WriteLine(person1);
+            Console.WriteLine(person2);
+            Console.WriteLine(person3);
+        }
+    }
+}
---
+++ Day 15/02Exercise/Program.cs
@@ -0,0 +1,108 @@
+﻿namespace _02Exercise
+{
+    class Order
+    {
+        public string Status;
+        private bool discount = false;
+
+        private string _customerName;
+        public string CustomerName
+        {
+            get { return _customerName; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _customerName = value;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong Customer Name");
+                }
+            }
+        }
+
+        private decimal _totalAmount;
+        public decimal TotalAmount
+        {
+            get { return _totalAmount; }
+
+        }
+
+        private int _orderId;
+        public int OrderId
+        {
+            get { return _orderId; }
+            set { _orderId = value; }
+        }
+
+        public Order()
+        {
+            _customerName = "not available";
+            Status = "NEW";
+        }
+
+        public Order(int orderid, string customername)
+        {
+            this.OrderId = orderid;
+            this.CustomerName = customername;
+            Status = "NEW";
+        }
+
+        public void AddItem(decimal price)
+        {
+            if (price < 0) Console.WriteLine("Amount can't be negative");
+            else _totalAmount += price;
+        }
+
+        public void ApplyDiscount(decimal percentage)
+        {
+            if (!discount && percentage <= 30 && percentage >= 0)
+            {
+                _totalAmount -= ((percentage / 100) * this.TotalAmount);
+                discount = true;
+            }
+            else
+            {
+                Console.WriteLine("Discount Already Applied");
+            }
+        }
+
+        public void GetOrderSummary()
+        {
+            Console.WriteLine($"Order Id = {_orderId}");
+            Console.WriteLine($"Customer = {_customerName}");
+            Console.WriteLine($"Total Amount = {_totalAmount}");
+            Console.WriteLine($"Status = {Status}");
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+            {
+                Console.WriteLine("ORDER 1");
+                Order order1 = new Order(201, "Sneha");
+                order1.AddItem(500);
+                order1.AddItem(300);
+                order1.ApplyDiscount(10);
+                order1.ApplyDiscount(20);
+                order1.GetOrderSummary();
+
+                Console.WriteLine("ORDER 2");
+                Order order2 = new Order();
+                order2.GetOrderSummary();
+
+                Console.WriteLine("ORDER 3");
+                Order order3 = new Order(202, null);
+                order3.GetOrderSummary();
+
+                Console.WriteLine("ORDER 4");
+                Order order4 = new Order(203, "Harpuneet");
+                order4.AddItem(500);
+                order4.AddItem(789);
+                order4.ApplyDiscount(10);
+                order4.GetOrderSummary();
+            }
+    }
+}
