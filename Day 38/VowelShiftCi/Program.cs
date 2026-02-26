054fedefa771f70407bd568db7ec4512e70fb79f
snehapahuja <snehapahuja604@gmail.com>
2026-02-26 22:46:23 +0530

    Day 38 commit

---
+++ Day 38/VowelShiftCi/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace VowelShiftCi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            string input = Console.ReadLine();
+            Dictionary<char, char> map = new Dictionary<char, char>()
+                        {
+                            {'a','e'},
+                            {'e','i'},
+                            {'i','o'},
+                            {'o','u'},
+                            {'u','a'}
+                        };
+
+            string res = "";
+
+            foreach (char c in input)
+            {
+                if (map.ContainsKey(c))   
+                {
+                    res += map[c];
+                }
+                else  
+                {
+                
+                    char next = (char)((c - 'a' + 1) % 26 + 'a');
+
+                
+                    if (map.ContainsKey(next))
+                    {
+                        next = (char)((next - 'a' + 1) % 26 + 'a');
+                    }
+
+                    res += next;
+                }
+            }
+
+            Console.WriteLine("Result: " + res);
+        }
+    }
+}
