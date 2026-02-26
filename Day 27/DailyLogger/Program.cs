2f512731004a28e904a4a2a1cf56da0e4d6a6909
snehapahuja <snehapahuja604@gmail.com>
2026-02-26 20:08:23 +0530

    Day 27 (2) commit

---
+++ Day 27/DailyLogger/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DailyLogger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"../../../journal.txt";
+            //using FileStream fs = new FileStream(fileName, FileMode.Create);
+            using StreamWriter sw = new StreamWriter(fileName, true);
+            do
+            {
+                Console.WriteLine("Enter Daily Reflection: ");
+                string data = Console.ReadLine();
+                if(data == "stop")
+                {
+                    break;
+                }
+                sw.WriteLine(data);
+            } while (true);
+        }
+    }
+}
