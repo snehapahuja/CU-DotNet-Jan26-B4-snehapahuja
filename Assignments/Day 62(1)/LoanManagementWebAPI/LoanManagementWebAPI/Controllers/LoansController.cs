58bab638711d0d4e680bc88a6725f3140c683f01
snehapahuja <snehapahuja604@gmail.com>
2026-03-20 12:22:11 +0530

    Day 62 commit

---
+++ Assignments/Day 62(1)/LoanManagementWebAPI/LoanManagementWebAPI/Controllers/LoansController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LoanManagementWebAPI.Data;
+using LoanManagementWebAPI.Models;
+
+namespace LoanManagementWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoansController : ControllerBase
+    {
+        private readonly LoanManagementWebAPIContext _context;
+
+        public LoansController(LoanManagementWebAPIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Loans
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Loan>>> GetLoan()
+        {
+            return await _context.Loan.ToListAsync();
+        }
+
+        // GET: api/Loans/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Loan>> GetLoan(int id)
+        {
+            var loan = await _context.Loan.FindAsync(id);
+
+            if (loan == null)
+            {
+                return NotFound();
+            }
+
+            return loan;
+        }
+
+        // PUT: api/Loans/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLoan(int id, Loan loan)
+        {
+            if (id != loan.LoanId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(loan).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LoanExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Loans
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Loan>> PostLoan(Loan loan)
+        {
+            _context.Loan.Add(loan);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLoan", new { id = loan.LoanId }, loan);
+        }
+
+        // DELETE: api/Loans/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLoan(int id)
+        {
+            var loan = await _context.Loan.FindAsync(id);
+            if (loan == null)
+            {
+                return NotFound();
+            }
+
+            _context.Loan.Remove(loan);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LoanExists(int id)
+        {
+            return _context.Loan.Any(e => e.LoanId == id);
+        }
+    }
+}
---
+++ Assignments/Day 62(1)/LoanManagementWebAPI/LoanManagementWebAPI/Models/Loan.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoanManagementWebAPI.Models
+{
+    public class Loan
+    {
+        public int LoanId { get; set; }
+        [Required]
+        public string BorrowerName { get; set; }
+        [Required]
+        public decimal Amount { get; set; }
+        [Required]
+        public int LoanTermMonths { get; set; }
+        [Required]
+        public bool IsApproved { get; set; }
+    }
+}
