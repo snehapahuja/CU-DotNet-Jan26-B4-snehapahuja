a99f6021a40be0dce65579d2eb6dffc5457d864f
snehapahuja <snehapahuja604@gmail.com>
2026-03-28 12:54:07 +0530

    week 12 assessment

---
+++ Assessments/Week 12 Assessment/SecureFleetManagement/IdentityService/Models/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace IdentityService.Models
+{
+    public class Login
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Assessments/Week 12 Assessment/SecureFleetManagement/TrackingService/Controllers/TrackingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace TrackingService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TrackingController : Controller
+    {
+        [HttpGet("gps")]
+        [Authorize(Roles = "Manager")]
+        public IActionResult GetGps()
+        {
+            return Ok("Sensitive GPS Data");
+        }
+    }
+}
